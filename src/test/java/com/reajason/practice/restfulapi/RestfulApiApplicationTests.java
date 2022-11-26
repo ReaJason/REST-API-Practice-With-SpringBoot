fbfe03f5c0c33aca541e7950e0412dc863ec311d
ReaJason <1587047405@qq.com>
2022-11-26 15:56:27 +0800

    init

---
+++ src/test/java/com/reajason/practice/restfulapi/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reajason.practice.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
